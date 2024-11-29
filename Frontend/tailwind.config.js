c655913f2c75528fa7956f87a99edf7d7a0cb72e
Newt Welch <77295065+newtwelch@users.noreply.github.com>
2024-11-30 00:19:13 +0800

    Control tab UI update

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
         './Razor/**/*.razor',
         './wwwroot/index.html',
     ],
+    safelist: [
+        {
+            pattern: /(bg|text|from|to)-percentage-p(|1|2|3|4|5|6|7|8|9|10)0/,
+            variants: ['before']
+        }
+    ],
     //darkMode: false, // or 'media' or 'class'
     theme: {
         colors: {
