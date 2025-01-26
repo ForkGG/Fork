dcb1a53c0b234d0dcabb3d0222b550549116d393
txtsd <code@ihavea.quest>
2025-01-26 14:26:59 +0530

    Add Waterfall
    Signed-off-by: txtsd <code@ihavea.quest>

---
+++ Backend/src/Adapters/Waterfall/WaterfallVersionList.cs
@@ -0,0 +1,9 @@
+namespace Fork.Adapters.Waterfall;
+
+public class WaterfallVersionList
+{
+    public required string project_id;
+    public required string project_name;
+    public required string[] version_groups;
+    public required string[] versions;
+}
